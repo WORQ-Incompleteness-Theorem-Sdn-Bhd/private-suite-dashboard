c46e63f7b6237c4312b6c56919344e9ddb737a7b
dayangnurnazihah <nazihah.mroslan@worq.space>
2025-08-29 11:31:16 +0800

    deploy backend

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,32 +1,18 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import dotenv from "dotenv";
+dotenv.config();
 
-import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin";
+import app from "./app";
+import { onRequest } from "firebase-functions/https";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.F1REBASE_PROJECT_ID,
+      clientEmail: process.env.F1REBASE_CLIENT_EMAIL,
+      privateKey: process.env.F1REBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const appFunction = onRequest({ region: "asia-southeast1" }, app);
